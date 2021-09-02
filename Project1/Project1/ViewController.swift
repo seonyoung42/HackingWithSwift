e2bd8bc7d310ee62170eb924c084b943cdc26ec7
seonyoung <4tjsdud2@naver.com>
2021-09-02 12:50:21 +0900

    Day 17 (navigationController)

--- Project1/Project1/ViewController.swift
+++ Project1/Project1/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        title = "Storm Viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
         let fm = FileManager.default
         let path = Bundle.main.resourcePath! // tells me where i can find all those images i added to my app
         let items = try! fm.contentsOfDirectory(atPath: path)
@@ -37,6 +40,13 @@ class ViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Picture",for: indexPath)
         cell.textLabel?.text = pictures[indexPath.row]
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(identifier: "Detail") as? DetailViewController {
+            vc.selectedImage = pictures[indexPath.row]
+            navigationController?.pushViewController(vc, animated: true)
+        }
     }
 }
 
