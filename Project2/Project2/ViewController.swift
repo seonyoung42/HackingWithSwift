330e8cc35e939c5a437d9a46e949e7f5d55493c7
seonyoung <4tjsdud2@naver.com>
2021-09-04 12:54:02 +0900

    Day 19

---
+++ Project2/Project2/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by 장선영 on 2021/09/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
+    var countries = [String]()
+    var score = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        countries += ["estonia","france","germany","ireland","italy","monaco","nigeria","poland","russia","spain","uk","us"]
+        
+//        countries.append("estonia")
+//        countries.append("france")
+//        countries.append("germany")
+//        countries.append("ireland")
+//        countries.append("italy")
+//        countries.append("monaco")
+//        countries.append("nigeria")
+//        countries.append("poland")
+//        countries.append("russia")
+//        countries.append("spain")
+//        countries.append("uk")
+//        countries.append("us")
+        
+        askQuestion()
+        
+        [button1,button2,button3].forEach {
+            $0?.layer.borderWidth = 1
+            $0?.layer.borderColor = UIColor.lightGray.cgColor 
+        }
+    }
+    
+    func askQuestion() {
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+    }
+
+
+}
+
