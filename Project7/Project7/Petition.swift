1eac576303a4fc0a891009aac111fe4961e2af15
seonyoung <4tjsdud2@naver.com>
2021-09-23 11:28:46 +0900

    Day 33 (Codable)

---
+++ Project7/Project7/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by 장선영 on 2021/09/23.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ Project7/Project7/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by 장선영 on 2021/09/23.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
