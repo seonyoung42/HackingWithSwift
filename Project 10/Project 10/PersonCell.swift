4fa281dcf9e23cc9b8444b88b1620f756a2b69ba
seonyoung <4tjsdud2@naver.com>
2021-10-03 16:08:52 +0900

    Day 42 (UICollectionView)

---
+++ Project 10/Project 10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Project 10
+//
+//  Created by 장선영 on 2021/10/03.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var namve: UILabel!
+}
