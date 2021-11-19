b4e979f84b225c579f709fff2998b8ff3677cba9
seonyoung <4tjsdud2@naver.com>
2021-11-19 12:57:32 +0900

    Day 55 (SKCropNode, SKAction)

---
+++ Project14/Project14/WhackSlot.swift
@@ -0,0 +1,66 @@
+//
+//  WhackSlot.swift
+//  Project14
+//
+//  Created by 장선영 on 2021/11/19.
+//
+
+import SpriteKit
+import UIKit
+
+class WhackSlot: SKNode {
+    
+    var charNode: SKSpriteNode!
+    
+    var isVisible = false
+    var isHit = false
+    
+    func configure(at position: CGPoint) {
+        self.position = position
+        let sprite = SKSpriteNode(imageNamed: "whackHole")
+        addChild(sprite)
+        
+        let cropNOde = SKCropNode()
+        cropNOde.position = CGPoint(x: 0, y: 15)
+        cropNOde.zPosition = 1
+        cropNOde.maskNode = SKSpriteNode(imageNamed: "whackMask")
+        
+        charNode = SKSpriteNode(imageNamed: "penguinGood")
+        charNode.position = CGPoint(x: 0, y: -90)
+        charNode.name = "character"
+        cropNOde.addChild(charNode)
+        
+        addChild(cropNOde)
+    }
+    
+    
+    func show(hideTime: Double) {
+        if isVisible { return }
+        
+        charNode.run(SKAction.moveBy(x: 0, y: 80, duration: 0.05))
+        isVisible = true
+        isHit = false
+        
+        if Int.random(in: 0...2) == 0 {
+            charNode.name = "charFriend"
+            charNode.texture = SKTexture(imageNamed: "penguinGood")
+        } else {
+            charNode.texture = SKTexture(imageNamed: "penguinEvil")
+            charNode.name = "charEnemy"
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + (hideTime * 3.5)) {
+            [weak self] in
+            self?.hide()
+        }
+    }
+    
+    func hide() {
+        if !isVisible {
+            return
+        }
+        
+        charNode.run(SKAction.moveBy(x: 0, y: -80, duration: 0.05))
+        isVisible = false
+    }
+}
