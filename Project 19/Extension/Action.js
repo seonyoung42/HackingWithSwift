0a28fae0cfab43de35f42769e42ee95ff2f1afe2
seonyoung <4tjsdud2@naver.com>
2021-12-06 14:57:53 +0900

    Day 67 (NSExtensionItem)

---
+++ Project 19/Extension/Action.js
@@ -0,0 +1,17 @@
+var Action = function() { }
+
+Action.prototype = {
+    
+run: function(parameters) {
+    
+},
+
+finalize: function(parameters) {
+    
+}
+    
+};
+
+var ExtensionPreprocessingJS = new Action
+
+
